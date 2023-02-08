c240b2e1e675febe7a08b10e73e5169790a67628
Katie Gumpertz <123984339+hcktg@users.noreply.github.com>
2023-02-07 18:42:11 -0600

    Maybe this test suite will work

--- tests/helloh1.ts
+++ tests/helloh1.ts
@@ -1,7 +1,21 @@
-import { test } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 const BASE_URL = process.env.MY_CUSTOM_BASE_URL;
 
-test('verify title of the page', async ({ page }) => {
-  await page.goto(BASE_URL);
+test.describe("basic testing", () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto(BASE_URL);
+  });
+  
+  test("has title", async ({ page }) => {
+    // Expect a title "to contain" a substring "webpage".
+    await expect(page).toHaveTitle(/webpage/);
+  });
+
+  test("has URL", async ({ page }) => {
+    // Assertions use the expect API.
+    await expect(page).toHaveURL(BASE_URL);
+  });
+  
 });
