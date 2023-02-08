1011c96158c67c2f6b141112624747a9a0a4b763
Katie Gumpertz <123984339+hcktg@users.noreply.github.com>
2023-02-07 18:47:15 -0600

    Add some logging for debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,3 +6,6 @@ const config = {
     baseUrl: process.env.MY_CUSTOM_BASE_URL
   }
 }
+
+// DEBUG LINE ONLY:  https://github.com/microsoft/playwright/issues/11060
+console.log('in playwright.config.ts, __dirname is', __dirname);
